6a9c2864911445067845ed1595092c1ed0e7839b
thamyresarm <pink.thamyres@gmail.com>
2023-05-07 18:02:27 -0300

    Aula 1 - criando componente

---
+++ react-fire-chat/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <div className="logo">
+          <div>
+            <img src="/logo.png" alt="Logo Firechat" />
+          </div>
+          <div>
+            <h1>Firechat</h1>
+            <h2>React Firebase Chat</h2>
+          </div>
+        </div>
+        <div className="user"></div>
+      </header>      
+    </div>
+  );
+}
+
+export default App;
