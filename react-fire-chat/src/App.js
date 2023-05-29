69811979824d53533ae6bd07d76d1faaa41b063c
Thamyres Magalhães <pink.thamyres@gmail.com>
2023-05-29 15:06:34 -0300

    corrigindo config firebird

--- react-fire-chat/src/App.js
+++ react-fire-chat/src/App.js
@@ -11,7 +11,7 @@ import SignOut from "./components/SignOut";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROTECTED_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
